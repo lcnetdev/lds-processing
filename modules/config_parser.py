c2be3cef2a41abf8d4cc8d9750b1a407e88b442d
Kevin Ford <kefo@loc.gov>
2019-08-14 18:19:03 -0400

    Change location of config file(s).

--- modules/config_parser.py
+++ modules/config_parser.py
@@ -2,7 +2,7 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description = 'Convert records/resources.')
-parser.add_argument('-c', '--config', default=os.path.dirname(__file__) + "/../config.yaml", help='Configuration file path.')
+parser.add_argument('-c', '--config', default=os.path.dirname(__file__) + "/../config/config.yaml", help='Configuration file path.')
 # parser.add_argument('-d', '--data', default="", help='Source data')
 
 args = parser.parse_args()
