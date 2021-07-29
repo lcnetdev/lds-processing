7df419f1a63e10e467a2b3027bf0a31a699ad5c0
Kevin Ford <kefo@loc.gov>
2021-07-29 19:17:30 -0400

    Support for sluriping iso2709 and posting to endpoint as marc/xml.

--- modules/config_parser.py
+++ modules/config_parser.py
@@ -3,6 +3,7 @@ import os
 
 parser = argparse.ArgumentParser(description = 'Convert records/resources.')
 parser.add_argument('-c', '--config', default=os.path.dirname(__file__) + "/../config/config.yaml", help='Configuration file path.')
+parser.add_argument('-s', '--since', default="", help='SINCE=YYYY-MM-DD|vNN.iNN')
 # parser.add_argument('-d', '--data', default="", help='Source data')
 
 args = parser.parse_args()
